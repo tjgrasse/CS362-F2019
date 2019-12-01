a9aca1b334360da5e5ccddae30a90f369464fb1a
T Grasse <grasset@oregonstate.edu>
2019-12-01 15:38:28 -0600

    FinalProject-Bugs unittest1.c

--- projects/FinalProject-Bugs/dominion/unittest1.c
+++ projects/FinalProject-Bugs/dominion/unittest1.c
@@ -17,15 +17,34 @@ void displayTestInfo()
     printf("***************************************************\n\n");
 }
 
+void testSetup(struct gameState *state, struct gameState *prevState)
+{
+    memset(state, 0, sizeof(struct gameState));
+
+    state->whoseTurn = 0;
+    state->handCount[0] = 2;
+    state->hand[0][0] = mine;
+    state->hand[0][1] = copper;
+    state->discardCount[0] = 0;
+    state->supplyCount[silver] = 10;
+
+    memcpy(prevState, state, sizeof(struct gameState));
+}
+
 int main(void)
 {
+    int result;
     struct gameState state, prevState;
 
     displayTestInfo();
 
-    memset(&state, 0, sizeof(struct gameState));
-    memset(&prevState, 0, sizeof(struct gameState));
-
+    testSetup(&state, &prevState);
+    result = cardEffect(mine, 1, silver, 0, &state, 0, 0);
+    testAssert("Assert 1", result, SUCCESS);
+    testAssert("Assert 2", state.discardCount[0], (prevState.discardCount[0] + 1));
+    testAssert("Assert 3", state.handCount[0], (prevState.handCount[0] - 1));
+    testAssert("Assert 4", state.discard[0][0], mine);
+    testAssert("Assert 5", state.hand[0][0], silver);
 
     printf("\n");
 }
